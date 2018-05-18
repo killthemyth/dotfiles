805dee87877826935deaae32cbf344bbeb470ff8
Kushal Keshri <kkeshri@adobe.com>
2018-05-18 18:55:29 +0530

    Major changes and moved to github vimrc

---
+++ my_configs.vim
@@ -0,0 +1,19 @@
+ set pastetoggle=<F2>
+ colorscheme morning
+
+ "Mapping in insert mode
+ imap jj <Esc>
+
+ "Mapping quit to QQ
+ map QQ :qall!<Enter>
+
+ "Fast quit and save from normal and insert mode
+ :map <C-X> <ESC>:q<CR>
+ :imap <C-X> <ESC>:q<CR>
+
+ "mapping : to ;
+ nore ; :
+
+ hi Search cterm=NONE ctermfg=grey ctermbg=blue
+
+ set nofoldenable
--- mytmux.sh
+++ mytmux.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
 # Setup a work space called `work` with two windows
-# first window has 3 panes. 
+# first window has 3 panes.
 # The first pane set at 65%, split horizontally, set to api root and running vim
-# pane 2 is split at 25% and running redis-server 
+# pane 2 is split at 25% and running redis-server
 # pane 3 is set to api root and bash prompt.
 # note: `api` aliased to `cd ~/path/to/work`
 #
@@ -15,10 +15,14 @@ tmux start-server
 # create a new tmux session, starting vim from a saved session in the new window
 tmux new-session -d -s $session -n vim #"vim -S ~/.vim/sessions/kittybusiness"
 
+# Select pane 1, set dir to api, run vim
+tmux selectp -t 1
+tmux send-keys "api;vim" C-m
+
 # create a new window called scratch
 #tmux new-window -t $session:1 -n scratch
-tmux new-window -t $session:2 -n docker 
-tmux new-window -t $session:3 -n code 
+tmux new-window -t $session:2 -n docker
+tmux new-window -t $session:3 -n code
 
 # return to main vim window
 tmux select-window -t $session:0
